a06f9e120571736eb754e4364f7fed64403af8ea
Martin Doucha <doucha@swarmtech.cz>
2021-08-15 21:23:25 +0200

    Add licence header to gamestate source files

--- src/gamestate.h
+++ src/gamestate.h
@@ -1,5 +1,24 @@
-#ifndef SAVEGAME_H_
-#define SAVEGAME_H_
+/*
+ * This file is part of OpenOrion2
+ * Copyright (C) 2021 Daniel Donisa, Martin Doucha
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GAMESTATE_H_
+#define GAMESTATE_H_
 
 #include "stream.h"
 #include <vector>
